117830ab352d404213e7ed9973cf1f647f5ff14b
Kate <katehappy16@gmail.com>
2023-11-29 14:53:05 +0300

    задача2

---
+++ задача2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+int [] FillArray(int size = 10)
+{
+    int [] array = new int [10];
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = new Random().Next(-100, 100); // в условии задачи не задан промежуток, от себя добавила именно такой
+    }
+    return array;
+}
+void PrintArray(int [] madeUpArray)
+{
+
+    for(int i =  0; i < madeUpArray.Length; i++)
+    {
+        Console.Write(madeUpArray[i] + " ");
+    }
+    Console.WriteLine();
+
+}
+void FindCountOfEvenNumbers (int [] arrayWeHave)
+{
+    int count = 0;
+    for (int i = 0; i < arrayWeHave.Length; i++)
+    {
+        if (arrayWeHave [i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Колличество чётных чисел в заданном массиве равно {count}");
+}
+
+int [] arr = FillArray(10);
+PrintArray(arr);
+FindCountOfEvenNumbers(arr);
