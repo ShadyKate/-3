7a3bf528ed9371c5d36eb8fb2c90805c2b17eed4
Kate <katehappy16@gmail.com>
2023-11-29 17:44:52 +0300

    задача3

---
+++ task3/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+double [] FillArray(int size) 
+{
+    Random random = new Random();
+    double [] array = new double [size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.NextDouble() * 1000; // так как нет указания из какого промежутка должны быть числа, оставляю до 1000
+    }
+    return array;
+}
+void PrintArray(double [] madeUpArray)
+{
+    for (int i = 0; i < madeUpArray.Length; i++)
+    {
+        Console.WriteLine(madeUpArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+void FindDiffMaxAndMin (double [] arrayWeHave)
+{
+    
+    int i = 0;
+    double max = arrayWeHave[i];
+    double min = arrayWeHave [i];
+    for(i = 0; i < arrayWeHave.Length; i++)
+        {
+            if (max > arrayWeHave[i])
+            {
+               min = arrayWeHave[i];
+
+            }
+            else 
+                if(arrayWeHave[i] > min)
+                    {
+                        max = arrayWeHave[i];
+                    }
+
+        
+        
+        }
+double diff = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным значением массива равна {diff}");
+}
+
+int arraySize = 10;
+double [] arr = FillArray(arraySize);
+PrintArray(arr);
+
+FindDiffMaxAndMin(arr);
